9b2912c2aadd9577f26143f1ddbdd7825073b11d
Aaron <85681241+27Aaron@users.noreply.github.com>
2023-05-27 22:34:00 +0800

    feat(security): 添加安全模块
    包含防火墙规则和 sudo 配置

---
+++ modules/core/security/firewall.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.security'.firewall;
+in {
+  options.security'.firewall = {
+    enable = lib.mkEnableOption "Enable the firewall";
+  };
+
+  config = lib.mkIf cfg.enable {
+    networking = {
+      firewall = {
+        enable = lib.mkDefault true;
+        allowPing = lib.mkDefault true;
+      };
+      nftables.enable = lib.mkDefault true;
+    };
+  };
+}
