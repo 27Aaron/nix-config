c53af619dd2fe28cfd22f0ad9545948ac8baa4c3
Aaron <85681241+27Aaron@users.noreply.github.com>
2023-05-27 21:16:00 +0800

    feat(flake): 初始化 flake 配置框架
    添加 flake.nix、default.nix 入口及模块索引

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Aaron's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    preservation.url = "github:nix-community/preservation";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    forEachSystem = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    nixosModules = import ./modules {inherit lib;};
+    nixosConfigurations = import ./hosts {inherit self inputs lib;};
+
+    # nix code formatter
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
+}
