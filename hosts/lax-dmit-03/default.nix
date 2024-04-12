b577423eb558281ad0ee08007fc7b0265254ba2f
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-12 19:30:00 +0800

    fix(hosts): lax-dmit-03 补充 proxy.nix 导入并规范配置结构

--- hosts/lax-dmit-03/default.nix
+++ hosts/lax-dmit-03/default.nix
@@ -1,17 +1,18 @@
-{
+{inputs, ...}: {
   imports = [
     ./network.nix
     ./hardware.nix
+    "${inputs.my-secrets}/network/lax-dmit-03/proxy.nix"
   ];
 
   # LAX DMIT Echo 1C-1G-20G
 
   # Infrastructure
-  services'.vnstat.enable = true;
-  services'.openssh.enable = true;
   security'.firewall.enable = true;
+  services'.openssh.enable = true;
+  services'.vnstat.enable = true;
 
-  # Application stack
+  # Proxy
   services'.sing-box.enable = true;
   services'.snell-server.enable = true;
 }
