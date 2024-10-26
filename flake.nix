745435dd73342139718be06771e19a6c1454927a
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-10-26 17:26:15 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Aaron's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence.url = "github:nix-community/impermanence";
+
+  };
+
+  outputs = inputs@{ nixpkgs, disko, impermanence,... }: {
+    nixosConfigurations = {
+      beelink = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          disko.nixosModules.disko
+          impermanence.nixosModules.impermanence
+        ];
+      };
+    };
+  };
+}
