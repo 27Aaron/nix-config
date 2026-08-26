cab2e2caddc29cdd6f99c74cc6939dda5b1662e4
Aaron <niceboy@duck.com>
2026-08-26 22:31:34 +0800

    style(nixos): format GTK theme configuration

--- modules/nixos/desktop/themes.nix
+++ modules/nixos/desktop/themes.nix
@@ -21,10 +21,10 @@ in {
       gtk = {
         enable = true;
 
-      theme = {
-        package = pkgs.adw-gtk3;
-        name = "adw-gtk3-dark";
-      };
+        theme = {
+          package = pkgs.adw-gtk3;
+          name = "adw-gtk3-dark";
+        };
 
         iconTheme = {
           package = pkgs.papirus-icon-theme;
