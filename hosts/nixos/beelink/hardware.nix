5186f7d6aa90072834fe5c687e7612c486f8c70b
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-08-26 13:19:06 +0800

    Merge pull request #1 from 27Aaron/feat/nixos-beelink
    feat(nixos): add Beelink host configuration

---
+++ hosts/nixos/beelink/hardware.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  hardware'.disko = {
+    enable = true;
+    device = "/dev/disk/by-id/nvme-CT1000P3PSSD8_24364AD5D8E0";
+    espSize = "1G";
+    luks.enable = true;
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
