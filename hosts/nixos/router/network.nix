2a218893d27c72f3295db09d46068ada124caf31
Aaron <niceboy@duck.com>
2026-08-30 18:58:20 +0800

    refactor(router): systemd-networkd 改用类型化选项

--- hosts/nixos/router/network.nix
+++ hosts/nixos/router/network.nix
@@ -9,13 +9,11 @@
 
   systemd.network.networks."10-eth0" = {
     matchConfig.Name = "eth0";
-    networkConfig = {
-      # IPv4 comes from the static address; IPv6 addresses and routes are
-      # obtained through DHCPv6 and router advertisements.
-      Address = "10.77.77.66/24";
-      Gateway = "10.77.77.1";
-      DNS = ["10.77.77.1"];
-      DHCP = "ipv6";
-    };
+    # IPv4 comes from the static address; IPv6 addresses and routes are
+    # obtained through DHCPv6 and router advertisements.
+    address = ["10.77.77.66/24"];
+    gateway = ["10.77.77.1"];
+    dns = ["10.77.77.1"];
+    networkConfig.DHCP = "ipv6";
   };
 }
